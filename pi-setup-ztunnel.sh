36992820171751d9baf81cc2c2e71deccc71bab8
npolshakova <nina.polshakova@solo.io>
2023-10-26 21:15:32 -0400

    pi setup fix

--- pi-setup-ztunnel.sh
+++ pi-setup-ztunnel.sh
@@ -11,12 +11,16 @@ fi
 # Store the provided address in a variable
 ISTIO_EW_ADDRESS="$1"
 
+sudo dpkg -i ztunnel_0.0.0-1_arm64.deb 
+
 # Setup pi files 
 cd pi-files
 
+sudo mkdir -p ./var/run/secrets/tokens ./var/run/secrets/istio
 sudo mkdir -p /etc/certs
+
 sudo cp root-cert.pem /etc/certs/root-cert.pem
-sudo  mkdir -p ./var/run/secrets/tokens
+sudo cp root-cert.pem ./var/run/secrets/istio/root-cert.pem
 sudo cp istio-token ./var/run/secrets/tokens/istio-token
 
 # Config setup for running sidecar
@@ -27,8 +31,6 @@ sudo cp mesh.yaml ./etc/istio/config/mesh
 echo "${ISTIO_EW_ADDRESS} istiod.istio-system.svc" | sudo tee -a /etc/hosts
 
 sudo mkdir -p ./etc/istio/proxy
-sudo chown -R istio-proxy ./var/lib/istio /etc/certs /etc/istio/proxy /etc/istio/config ./var/run/secrets /etc/certs/root-cert.pem ./var/run/secrets/istio/root-cert.pem
-
-sudo dpkg -i ztunnel_0.0.0-1_arm64.deb  
+sudo chown -R istio-proxy ./var/lib/istio /etc/certs ./etc/istio/proxy ./etc/istio/config ./var/run/secrets /etc/certs/root-cert.pem ./var/run/secrets/istio/root-cert.pem 
 
 sudo -u istio-proxy CA_ADDRESS="istiod.istio-system.svc:15012" XDS_ADDRESS="istiod.istio-system.svc:15012" CLUSTER_ID=cluster1 NETWORK=vm-network RUST_LOG=debug ISTIO_META_ENABLE_HBONE=true ISTIO_META_DNS_CAPTURE=true ISTIO_META_DNS_AUTO_ALLOCATE=true ISTIO_META_DNS_PROXY_ADDR="127.0.0.1:15053" ztunnel
