e99ee288101a3b152f260afaed072e07344e4b1a
npolshakova <nina.polshakova@solo.io>
2023-10-30 22:10:08 -0400

    create ns conditionally

--- example_apps/example-apps-install.sh
+++ example_apps/example-apps-install.sh
@@ -8,12 +8,12 @@ kubectl label namespace default istio.io/dataplane-mode=ambient
 kubectl apply -f $COMMON_SCRIPTS/sleep/notsleep.yaml -n default
 
 # in ambient mode
-kubectl get namespace bookinfo &> /dev/null || kubectl create namespace bookinfo
+kubectl create namespace bookinfo --dry-run=client -o yaml | kubectl apply -f -
 kubectl label namespace bookinfo istio.io/dataplane-mode=ambient
 kubectl apply -f $COMMON_SCRIPTS/bookinfo/bookinfo.yaml -n bookinfo
 
 # in sidecar mode
-kubectl get namespace helloworld &> /dev/null || kubectl create namespace helloworld
+kubectl create namespace helloworld --dry-run=client -o yaml | kubectl apply -f -
 kubectl label namespace helloworld istio-injection=enabled --overwrite=true
 kubectl apply -f $COMMON_SCRIPTS/helloworld/helloworld.yaml -n helloworld
 
